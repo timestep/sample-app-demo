bb1b0e2c0decb42111af20ef635aa546dd38275d
timestep <jbosshellokitty@gmail.com>
2013-06-27 19:50:19 -0400

    refactoring tests, before statrements

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,40 +18,34 @@ describe "Static pages" do
   end
 
   describe "Help page" do
-
+    before {visit help_path}
     it "should have the h1 'Help'" do
-      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit help_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
   describe "About page" do
-
+    before {visit about_path}
     it "should have the h1 'About Us'" do
-      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit about_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 
   describe "Contact page" do
-
+    before {visit contact_path}
     it "should have the content 'Contact'" do
-      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit contact_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
