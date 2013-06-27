c409264e76f6e3b8793df0b1e64010f7ada5f6f4
timestep <jbosshellokitty@gmail.com>
2013-06-27 15:52:49 -0400

    wrote test to determine title exsistance

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,6 +16,13 @@ describe "StaticPages" do
   		visit '/static_pages/home'
   		page.should have_content('sample app')
   	end
+  	
+  	it 'should have the right title' do
+  		visit '/static_pages/home' 
+  		page.should have_selector('title',
+  			:text => "Ruby on Rails Tutorial Sample App | Home")
+  	end
+
   end
 
   describe 'Help page' do
@@ -23,6 +30,13 @@ describe "StaticPages" do
   		visit '/static_pages/help'
   		page.should have_content('help')
   	end
+
+  	
+  	it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 
   describe 'about page' do
@@ -30,7 +44,13 @@ describe "StaticPages" do
   		visit '/static_pages/about'
   		page.should	 have_content ('About Us')
   	end
-  end
 
+  	it "should have the title 'Help'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+
+  end
 
 end
