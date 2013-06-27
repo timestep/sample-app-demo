380566b1459e9d4774524201038d1aafa01863ea
timestep <jbosshellokitty@gmail.com>
2013-06-27 19:53:55 -0400

    refactoring testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
   subject { page }
 
   describe "Home page" do
@@ -8,38 +9,27 @@ describe "Static pages" do
 
     it { should have_content('Sample App') }
     it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
-    before {visit help_path}
-    it "should have the h1 'Help'" do
-      expect(page).to have_content('Help')
-    end
+    before { visit help_path }
 
-    it "should have the title 'Help'" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
-    before {visit about_path}
-    it "should have the h1 'About Us'" do
-      expect(page).to have_content('About Us')
-    end
+    before { visit about_path }
 
-    it "should have the title 'About Us'" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
-    before {visit contact_path}
-    it "should have the content 'Contact'" do
-      expect(page).to have_content('Contact')
-    end
+    before { visit contact_path }
 
-    it "should have the title 'Contact'" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
