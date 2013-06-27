aba0c92de804f34314dfcd161c6806ece8af8377
timestep <jbosshellokitty@gmail.com>
2013-06-27 19:53:32 -0400

    refactor testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 describe "Static pages" do
   subject { page }
+
   describe "Home page" do
-    before {visit root_path}
+    before { visit root_path }
+
     it { should have_content('Sample App') }
-    it { should have_title("Ruby on Rails Tutorial Sample App") }
-    it { should_not have_title('| Home') }
+    it { should have_title(full_title('')) }
   end
 
   describe "Help page" do
