566250c6879c750be70ddac1f3830f59f4f34b73
timestep <jbosshellokitty@gmail.com>
2013-06-27 15:55:34 -0400

    failing test for titles...

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -45,7 +45,7 @@ describe "StaticPages" do
   		page.should	 have_content ('About Us')
   	end
 
-  	it "should have the title 'Help'" do
+  	it "should have the title 'aboutus'" do
       visit '/static_pages/about'
       page.should have_selector('title',
                         :text => "Ruby on Rails Tutorial Sample App | About Us")
