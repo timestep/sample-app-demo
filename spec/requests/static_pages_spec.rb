0bda13444beed9378b11f55258680f6b8539f8c2
timestep <jbosshellokitty@gmail.com>
2013-06-27 16:02:48 -0400

    updated test for ruby 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,8 +12,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+       expect(page).to have_title ("Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -26,8 +25,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Help")
+       expect(page).to have_title ("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
@@ -40,8 +38,7 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | About Us")
+       expect(page).to have_title ("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
