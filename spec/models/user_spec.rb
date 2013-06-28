46273c0aa8dec8d2f51a09c34e0bb7e70e270b65
timestep <jbosshellokitty@gmail.com>
2013-06-28 10:56:09 -0400

    formatting

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,6 @@ describe User do
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
-
   	describe "when name is not present" do
   		before { @user.name = " " }
     	it { should_not be_valid }
@@ -32,15 +31,15 @@ describe User do
     	it { should_not be_valid }
   	end
 
-  	describe "when email format is invalid" do
-		it "should be invalid" do
-			addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-			foo@bar_baz.com foo@bar+baz.com]
-			addresses.each do |invalid_address|
-				@user.email = invalid_address
-				expect(@user).not_to be_valid
-			end
-		end
+	describe "when email format is invalid" do
+  	it "should be invalid" do
+  		addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+  		foo@bar_baz.com foo@bar+baz.com]
+  		addresses.each do |invalid_address|
+  			@user.email = invalid_address
+  			expect(@user).not_to be_valid
+  		end
+  	end
 	end
 
 	describe "when email format is valid" do
