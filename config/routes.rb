36741858b596a6fa2c98315b06f4f657c7353bfc
timestep <jbosshellokitty@gmail.com>
2013-06-28 11:55:15 -0400

    css for sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 SampleApp::Application.routes.draw do
 
 	resources :users	
-  
-  root to: 'static_pages#home'
-  
-  match '/signup',  to: 'users#new',            via: 'get'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
+
+	root to: 'static_pages#home'
+
+	match '/signup',  to: 'users#new',            via: 'get'
+	match '/help',    to: 'static_pages#help',    via: 'get'
+	match '/about',   to: 'static_pages#about',   via: 'get'
+	match '/contact', to: 'static_pages#contact', via: 'get'
 
 
 end
