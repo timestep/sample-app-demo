92a931fb71f4bc801736968088108f3c29a4f444
timestep <jbosshellokitty@gmail.com>
2013-06-28 11:33:26 -0400

    added show user method and controller. and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 SampleApp::Application.routes.draw do
 
-  get "users/new"
-
+	resources :users	
+  
   root to: 'static_pages#home'
+  
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
-  resources :users
+
 end
