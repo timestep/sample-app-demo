f1dc13fb4fc772821566c47ec4df543b1e67287b
timestep <jbosshellokitty@gmail.com>
2013-06-28 17:15:35 -0400

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 SampleApp::Application.routes.draw do
 
-	resources :users	
+  	resources :users
 	resources :sessions, only: [:new, :create, :destroy]
 
-	root to: 'static_pages#home'
-
-	match '/signup',  to: 'users#new',			  via: 'post'
-	match '/signin',  to: 'sessions#new',		  via: 'post'
-  	match '/signout', to: 'sessions#destroy', 	  via: :delete
+	match '/signup',  to: 'users#new', 		via: 'get'
+	match '/signin',  to: 'sessions#new',	via: 'get'
+	match '/signout', to: 'sessions#destroy', via: :delete
 
 	match '/help',    to: 'static_pages#help',    via: 'get'
 	match '/about',   to: 'static_pages#about',   via: 'get'
