4d71ce3f84f51b02734b51770b154e2b0bb9584e
timestep <jbosshellokitty@gmail.com>
2013-06-28 18:05:32 -0400

    added attr asseccor and privat emethod in user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,23 @@
 class User < ActiveRecord::Base
-before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence:   true,
-                    format:     { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  has_secure_password
-  validates :password_confirmation, presence: true
-  validates :password, length: { minimum: 6 }
+
+	attr_accessible :name, :email, :password, :password_confirmation
+  	
+	before_save { self.email = email.downcase }
+	before_save :create_remember_token
+
+	validates :name, presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence:   true,
+	                format:     { with: VALID_EMAIL_REGEX },
+	                uniqueness: { case_sensitive: false }
+	has_secure_password
+	validates :password_confirmation, presence: true
+	validates :password, length: { minimum: 6 }
+
+	private
+
+	    def create_remember_token
+	      self.remember_token = SecureRandom.urlsafe_base64
+	    end
+
 end
