2b967dc49f6b2fe7c7af906caf4bb78211136ce0
timestep <jbosshellokitty@gmail.com>
2013-06-27 21:06:49 -0400

    all test faillssss

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,20 @@
 require 'spec_helper'
 
 describe User do
-	before do
-    	@user = User.new(name: "Example User", email: "user@example.com")
-  	end
+	  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
-	subject { @user }
+  subject { @user }
 
-	it { should respond_to(:name)}
-	it { should respond_to(:email)}
-	it { should respond_to(:password_digest) }
-	it { should be_valid }
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
 
   	describe "when name is not present" do
   		before { @user.name = " " }
@@ -65,6 +69,27 @@ describe User do
 
     it { should_not be_valid }
   end
-  
+
+
+  describe "when password is not present" do
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: " ", password_confirmation: " ")
+  end
+  it { should_not be_valid }
+end
+
+describe "when password doesn't match confirmation" do
+  before { @user.password_confirmation = "mismatch" }
+  it { should_not be_valid }
+end
+
+describe "when password confirmation is nil" do
+  before do
+    @user = User.new(name: "Michael Hartl", email: "mhartl@example.com",
+                     password: "foobar", password_confirmation: nil)
+  end
+  it { should_not be_valid }
+end
 
 end
