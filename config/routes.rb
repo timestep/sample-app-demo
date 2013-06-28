9004769dcf12fe83bc538424c01a1f6bbf5bf4fb
timestep <jbosshellokitty@gmail.com>
2013-06-28 11:28:41 -0400

    added resrouces route for useres

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ SampleApp::Application.routes.draw do
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
+  resources :users
 end
