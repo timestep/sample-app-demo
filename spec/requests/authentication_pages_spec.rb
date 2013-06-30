64532fbdd413b2bbbe7f3757ceb50844169684f0
timestep <jbosshellokitty@gmail.com>
2013-06-30 18:42:24 -0400

    fixed broken end statements in tests

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -55,49 +55,55 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
-       describe "when attempting to visit a protected page" do
-        before do
-          visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
-        end
-
-        describe "after signing in" do
-
-          it "should render the desired protected page" do
-            expect(page).to have_title('Edit user')
-          end
-        end
+     describe "when attempting to visit a protected page" do
+      before do
+        visit edit_user_path(user)
+        fill_in "Email",    with: user.email
+        fill_in "Password", with: user.password
+        click_button "Sign in"
       end
 
-      describe "in the Users controller" do
+      describe "after signing in" do
 
-        describe "visiting the edit page" do
-          before { visit edit_user_path(user) }
-          it { should have_title('Sign in') }
-        end
-
-        describe "submitting to the update action" do
-          before { patch user_path(user) }
-          specify { expect(response).to redirect_to(signin_path) }
+        it "should render the desired protected page" do
+          expect(page).to have_title('Edit user')
         end
       end
     end
-    describe "as wrong user" do
-      let(:user) { FactoryGirl.create(:user) }
-      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-      before { sign_in user }
 
-      describe "visiting Users#edit page" do
-        before { visit edit_user_path(wrong_user) }
-        it { should_not have_title(full_title('Edit user')) }
+    describe "in the Users controller" do
+
+      describe "visiting the user index" do
+        before { visit users_path }
+          it { should have_title('Sign in') }
+        end
       end
 
-      describe "submitting a PATCH request to the Users#update action" do
-        before { patch user_path(wrong_user) }
-        specify { expect(response).to redirect_to(root_path) }
+      describe "visiting the edit page" do
+        before { visit edit_user_path(user) }
+        it { should have_title('Sign in') }
       end
+
+      describe "submitting to the update action" do
+        before { patch user_path(user) }
+        specify { expect(response).to redirect_to(signin_path) }
+      end
+    end
+  end
+
+  describe "as wrong user" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+    before { sign_in user }
+
+    describe "visiting Users#edit page" do
+      before { visit edit_user_path(wrong_user) }
+      it { should_not have_title(full_title('Edit user')) }
+    end
+
+    describe "submitting a PATCH request to the Users#update action" do
+      before { patch user_path(wrong_user) }
+      specify { expect(response).to redirect_to(root_path) }
     end
   end
 end
