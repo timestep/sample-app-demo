9b310fa485be50740f818f21ec3638aa9ac94974
timestep <jbosshellokitty@gmail.com>
2013-06-30 16:48:22 -0400

    modifications to tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -15,8 +15,8 @@ describe "User pages" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
-    it { should have_selector('h1',    text: @user.name) }
-    it { should have_selector('title', text: @user.name) }
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
   end
 
   describe "signup" do
