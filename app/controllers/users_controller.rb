68fc1dd3b8c169d0e05a586f506037f586853bf8
timestep <jbosshellokitty@gmail.com>
2013-06-30 18:20:33 -0400

    added private methods for signedin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(new_params)
+    if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       sign_in @user
       redirect_to @user
@@ -39,4 +39,14 @@ class UsersController < ApplicationController
   	def new_params
   		params.require(:user).permit!
   	end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+    # Before filters
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
 end
