4c2888e5a9cb981a54710fbd47cee0dcdcbbf74c
timestep <jbosshellokitty@gmail.com>
2013-06-30 18:08:48 -0400

    updated update in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,13 +27,13 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
     else
       render 'edit'
     end
   end
-
-
 
   private
   	def new_params
