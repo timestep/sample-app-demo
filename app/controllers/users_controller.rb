fed57d32fbbae0d1ca86b784a62fb33a2197586e
timestep <jbosshellokitty@gmail.com>
2013-06-30 18:05:58 -0400

    added updated test with valid infor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,20 @@ class UsersController < ApplicationController
     end
   end
 
-  def end
-  
+  def edit
+    @user = User.find(params[:id])
   end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
 
 
   private
