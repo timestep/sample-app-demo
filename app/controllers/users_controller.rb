d291b6717839818e92247fa6452cd49640d982df
timestep <jbosshellokitty@gmail.com>
2013-06-30 20:25:17 -0400

    added signed in user only access index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]  
+  before_action :signed_in_user, only: [:index, :edit, :update] 
   before_action :correct_user,   only: [:edit, :update]
 	
 
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
  	def create
