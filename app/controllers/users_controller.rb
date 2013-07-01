db95184445355868ba26c38e2edabe1c221a26e4
timestep <jbosshellokitty@gmail.com>
2013-06-30 20:44:20 -0400

    adding destroy method for admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update] 
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy] 
   before_action :correct_user,   only: [:edit, :update]
 	
 
@@ -30,6 +30,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def destroy 
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
 
   def update
      if @user.update_attributes(new_params)
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -68,6 +68,18 @@ describe "Authentication" do
         end
       end
     end
+    
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_path) }
+      end
+    end
 
     describe "in the Users controller" do
 
