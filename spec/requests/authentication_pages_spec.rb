3d13bcfbb1d9da06a369f25bc332da1926ec0955
timestep <jbosshellokitty@gmail.com>
2013-06-30 20:05:52 -0400

    modifiying tests

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,6 +4,18 @@ describe "Authentication" do
 
   subject { page }
 
+  describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      it { should have_title(user.name) }
+      it { should have_link('Users',       href: users_path) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
+      it { should have_link('Sign out',    href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+  end
+
   describe "signin page" do
     before { visit signin_path }
 
@@ -18,22 +30,10 @@ describe "Authentication" do
 	        it { should_not have_selector('div.alert.alert-error') }
 	   	end
     end
-  
-    describe "with valid information" do
-      let(:user) { FactoryGirl.create(:user) }
-      before { sign_in user }
-
-      it { should have_title(user.name) }
-      it { should have_link('Users',       href: users_path) }
-      it { should have_link('Profile',     href: user_path(user)) }
-      it { should have_link('Settings',    href: edit_user_path(user)) }
-      it { should have_link('Sign out',    href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
-    
-      describe 'followed by signout' do
-        before { click_link 'Sign out' }
-        it{ should have_link('Sign in')}
-      end
+        
+    describe 'followed by signout' do
+      before { click_link 'Sign out' }
+      it{ should have_link('Sign in')}
     end
   end
 
