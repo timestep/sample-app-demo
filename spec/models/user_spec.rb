00f68a105a5ddcd8646004a8359432d8f9752fd4
timestep <jbosshellokitty@gmail.com>
2013-06-30 20:31:57 -0400

    beginning administrator

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe User do
-	  before do
-    @user = User.new(name: "Example User", email: "user@example.com",
+  before do
+  @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
 
@@ -15,6 +15,20 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "remember token" do
     before { @user.save }
